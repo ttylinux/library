7eb20c76a2ceff16cbbf2fddf4ad55aa5e2da00b
Lushuwei(Albert) <albertxiaoyu@163.com>
2014-10-17 17:14:10 +0800

    修改代码组织结构

--- AndroidLibrary/src/com/androidlibrary/lib/LibConstant.java
+++ AndroidLibrary/src/com/androidlibrary/lib/LibConstant.java
@@ -3,13 +3,12 @@
 *  创建时间 2014-10-17
 */
 
-package com.androidlibrary.lib.util;
+package com.androidlibrary.lib;
 
 import android.net.Uri;
 
-public class Constant {
+public class LibConstant {
 
-	
 	public static final Uri DownloadInfoDataBase_ContentUri =Uri.parse("content://downloads/my_downloads");
 }
 
