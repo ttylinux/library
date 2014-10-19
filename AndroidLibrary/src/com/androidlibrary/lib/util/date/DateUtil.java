a59a1e3cdbb4771ce2c51e7e358dbc3e80d32d60
LuShuWei <albertxiaoyu@163.com>
2014-10-19 13:18:25 +0800

    添加返回月份所在的季度方法

--- AndroidLibrary/src/com/androidlibrary/lib/util/date/DateUtil.java
+++ AndroidLibrary/src/com/androidlibrary/lib/util/date/DateUtil.java
@@ -17,40 +17,40 @@ public class DateUtil {
 	public static final int Jan = 1;
 	public static final int Dec = 12;
 	public static final int WeekLength = 7;
-	public static final int PartNumber = 3;
-	public static HashMap<Integer, String> seasonQ = new HashMap<Integer, String>();
-	public static HashMap<Integer, Integer> monthDay = new HashMap<Integer, Integer>();
+	public static final int PartCount = 3;
+	public static HashMap<Integer, String> _seasonQ = new HashMap<Integer, String>();
+	public static HashMap<Integer, Integer> _monthDay = new HashMap<Integer, Integer>();
 
 	static {
 
-		monthDay.put(1, 31);
-		monthDay.put(2, 28);
-		monthDay.put(3, 31);
-		monthDay.put(4, 30);
-		monthDay.put(5, 31);
-		monthDay.put(6, 30);
-		monthDay.put(7, 31);
-		monthDay.put(8, 31);
-		monthDay.put(9, 30);
-		monthDay.put(10, 31);
-		monthDay.put(11, 30);
-		monthDay.put(12, 31);
+		_monthDay.put(1, 31);
+		_monthDay.put(2, 28);
+		_monthDay.put(3, 31);
+		_monthDay.put(4, 30);
+		_monthDay.put(5, 31);
+		_monthDay.put(6, 30);
+		_monthDay.put(7, 31);
+		_monthDay.put(8, 31);
+		_monthDay.put(9, 30);
+		_monthDay.put(10, 31);
+		_monthDay.put(11, 30);
+		_monthDay.put(12, 31);
 
-		seasonQ.put(1, "1");
-		seasonQ.put(2, "1");
-		seasonQ.put(3, "1");
+		_seasonQ.put(1, "1");
+		_seasonQ.put(2, "1");
+		_seasonQ.put(3, "1");
 
-		seasonQ.put(4, "2");
-		seasonQ.put(5, "2");
-		seasonQ.put(6, "2");
+		_seasonQ.put(4, "2");
+		_seasonQ.put(5, "2");
+		_seasonQ.put(6, "2");
 
-		seasonQ.put(7, "3");
-		seasonQ.put(8, "3");
-		seasonQ.put(9, "3");
+		_seasonQ.put(7, "3");
+		_seasonQ.put(8, "3");
+		_seasonQ.put(9, "3");
 
-		seasonQ.put(10, "4");
-		seasonQ.put(11, "4");
-		seasonQ.put(12, "4");
+		_seasonQ.put(10, "4");
+		_seasonQ.put(11, "4");
+		_seasonQ.put(12, "4");
 	}
 
 	/**
@@ -69,7 +69,7 @@ public class DateUtil {
 		String startMonth = "";
 		String startDay = "";
 
-		if (end.length < PartNumber) {
+		if (end.length < PartCount) {
 			throw new Exception("param is invalid");
 		} else {
 			int year = Integer.valueOf(end[0]);
@@ -87,13 +87,13 @@ public class DateUtil {
 				{
 					startYear = (year - 1) + "";
 					startMonth = Dec+"";
-					startDay = (monthDay.get(Dec) - (WeekLength - day) + 1)
+					startDay = (_monthDay.get(Dec) - (WeekLength - day) + 1)
 							+ "";
 				}else
 				{
 					startYear = year + "";
 					startMonth = (month - 1) + "";
-					startDay = (monthDay.get(Integer.valueOf(startMonth))
+					startDay = (_monthDay.get(Integer.valueOf(startMonth))
 							- (WeekLength - day) + 1)
 							+ "";
 				}
@@ -110,7 +110,7 @@ public class DateUtil {
 	}
 
 	/**
-	 * convert date to strArray
+	 * convert date to strArray,use a new format "yyyy-m-d"
 	 * 
 	 * @param date
 	 *            format"yyyy-mm-dd"
@@ -131,10 +131,21 @@ public class DateUtil {
 
 	public static void setFeb(int year) {
 		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-			monthDay.put(2, 29);
+			_monthDay.put(2, 29);
 		} else {
-			monthDay.put(2, 28);
+			_monthDay.put(2, 28);
 		}
 	}
+	/**
+	 *determine the month belongs to which quarter.
+	 *@param  month 
+	 * 
+	 * @return  quarter 
+	 */
+	public static String determineWhichQuarter(int month)
+	{
+		return _seasonQ.get(month);
+	}
+	
 
 }
